6d35531cc613c14bff0288798bf4d0fb36becc8f
m3 <m3@localhost.localdomain>
2017-05-25 21:23:22 -0400

    Initial commit
    trying to get amenities and fill database with it
    
    commented out the part to go get  postGIS geom data and fill the db with it

---
+++ DB query/insert-amenities.sql
@@ -0,0 +1,6 @@
+﻿INSERT into parks.amenities (id,
+                              park_id,
+                              amenity_type_id,
+                              description,osm_info_type,
+                              osm_info_id)
+VALUES (1,12,9,'playground', 'way', 25668193);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# nrv-park-finder-scripts
-Backend scripts for the NRV Park Finder website
+# NRV-Park-Finder
+Find a public park and amenities in the New River Valley (VA)
+
+Will use OpenStreetMap curated through a Google Sheet/PostGIS database
